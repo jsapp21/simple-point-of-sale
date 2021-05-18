b2de36214e8192813ae12122cd7c3a8d626f7819
James Sapp <jameseisapp@gmail.com>
2021-05-17 22:42:36 -0500

    cat post req

--- simple-pos-backend/app/controllers/categories_controller.rb
+++ simple-pos-backend/app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        byebug
+        categories = Categories.all 
+
+        render json: categories
+    end 
+
+    def create
+        byebug
+        category = Categories.create(name: params[:name])
+
+        render json: category
+    end
+
 end
