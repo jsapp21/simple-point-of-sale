473b0294f3d4b49149bb1c11306f1cf8673443c1
James Sapp <jameseisapp@gmail.com>
2021-05-28 22:45:48 -0500

    size form

--- simple-pos-backend/app/controllers/sizes_controller.rb
+++ simple-pos-backend/app/controllers/sizes_controller.rb
@@ -1,2 +1,23 @@
 class SizesController < ApplicationController
+    skip_before_action :verify_authenticity_token    
+
+    def index
+        sizes = Size.all 
+
+        render json: sizes
+    end
+
+    def create
+        size = Size.create(size_params)
+
+        render json: size
+    end
+
+    private
+
+    def size_params
+        params.require(:size).permit(:name, :price, :product_id)
+    end
+
 end
+# need to fix the tables price to a float
--- simple-pos-backend/app/serializers/product_serializer.rb
+++ simple-pos-backend/app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  # has_many :orders, :allergies, :ingredients, :modifications, :sizes
+  # has_many :orders, :allergies, :ingredients, :modifications, :size
   belongs_to :category
   
   attributes :id, :name, :description
